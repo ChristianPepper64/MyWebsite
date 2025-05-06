572e648c41570a6f63b33bf8fd6c806bd8455740
ChristianPepper64 <pepperca@jacks.sfasu.edu>
2025-05-05 19:48:56 -0500

    Page crestion

---
+++ MyWebsite_Web/Pages/NavProperties/Education.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebsite_Web.Pages.NavPoperties
+{
+    public class ResumeInformationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ MyWebsite_Web/Pages/NavProperties/ExperiencePage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebsite_Web.Pages.NavPoperties
+{
+    public class ExperiencePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
