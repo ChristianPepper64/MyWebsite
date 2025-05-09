160a8f162e680107dd1023c0b120ce6ef24ae46a
ChristianPepper64 <pepperca@jacks.sfasu.edu>
2025-05-09 13:16:48 -0500

    GAME

---
+++ MyWebsite_Web/Pages/NavProperties/GamePage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebsite_Web.Pages.NavProperties
+{
+    public class GamePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
