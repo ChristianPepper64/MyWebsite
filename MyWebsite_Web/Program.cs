be9f92fe3831ee06ec835ca66703ea07c830aed4
ChristianPepper64 <pepperca@jacks.sfasu.edu>
2025-05-17 02:40:25 -0500

    Finishing projects

--- MyWebsite_Web/Program.cs
+++ MyWebsite_Web/Program.cs
@@ -26,7 +26,7 @@ app.UseStaticFiles(new StaticFileOptions
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days.
     app.UseHsts();
 }
 
